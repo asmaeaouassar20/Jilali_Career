a5963254adeb2da02a8e35efa552294506daf811
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-07 17:15:43 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'layout',
+        component: Layout,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            }
+        ]
+    }
+];

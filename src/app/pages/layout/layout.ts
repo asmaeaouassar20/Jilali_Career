827d3c59e9b0d5e86e9886a9cffa363630407262
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-08 11:58:48 +0100

    logout

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IUser } from '../../core/model/interfaces/User.model';
 import { Global } from '../../core/constant/Global.constant';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -10,10 +11,15 @@ import { Global } from '../../core/constant/Global.constant';
 })
 export class Layout {
   loggedUser! : IUser;
+  router = inject(Router);
   constructor(){
     const localUser = localStorage.getItem(Global.LOGIN_LOCAL_KEY);
     if(localUser != null){
       this.loggedUser = JSON.parse(localUser);
     }
+  }
+  onLogout(){
+    localStorage.removeItem(Global.LOGIN_LOCAL_KEY);
+    this.router.navigate(["/home"]);
   }
 }

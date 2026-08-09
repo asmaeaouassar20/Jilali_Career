2aa1075882d0ed877fdb662e02c40299d39dd193
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-09 14:10:02 +0100

    display notes

--- src/app/pages/add-note/add-note.ts
+++ src/app/pages/add-note/add-note.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NoteModel } from '../../core/model/classes/Note.model';
 import { Router } from '@angular/router';
 import { AddNoteModal } from "../../components/add-note-modal/add-note-modal";
@@ -10,12 +10,35 @@ import { AddNoteModal } from "../../components/add-note-modal/add-note-modal";
   templateUrl: './add-note.html',
   styleUrl: './add-note.css',
 })
-export class AddNote {  
+export class AddNote implements OnInit {  
   newUserNote = new NoteModel(); // initialiser la note
   noteService = inject(Router);
 
   // modal to add new note
   isModalAddNoteOpen : boolean = false;
+
+  myNotes! : NoteModel[];
+
+  ngOnInit() : void {
+    this.loadAllNotes();
+  }
+
+  loadAllNotes(){
+    const notes = localStorage.getItem('my-notes');
+    if(notes){
+      this.myNotes=JSON.parse(notes)
+    }else{
+      this.myNotes=this.factoryNotes();
+    }
+  }
+
+  factoryNotes() : NoteModel[]{
+    const newNotes : NoteModel[] = [
+      {id:100 , title: "Asmae" , content:"I am beauriful and intelligent" , createdAt: new Date()},
+      {id:200 , title: "ALi" , content:"I am a vendor" , createdAt:new Date()}
+    ]
+    return newNotes
+  }
 
   openModalAddNote(){
     this.isModalAddNoteOpen=true;

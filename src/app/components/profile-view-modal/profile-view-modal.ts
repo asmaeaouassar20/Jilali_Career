99b01c22df715066fa29133b271c98578bfa5eab
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-30 22:28:28 +0100

    get user infos from localstorage

--- src/app/components/profile-view-modal/profile-view-modal.ts
+++ src/app/components/profile-view-modal/profile-view-modal.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { CurrentUserService } from '../../core/services/currentuser/current-user-service';
+import { IUser } from '../../core/model/interfaces/User.model';
 
 @Component({
   selector: 'app-profile-view-modal',
@@ -6,9 +8,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './profile-view-modal.html',
   styleUrl: './profile-view-modal.css',
 })
-export class ProfileViewModal {
+export class ProfileViewModal implements OnInit {
+
+  private connectedUserService = inject(CurrentUserService);
+  connectedUser! : IUser | null;
+
   @Input() isOpen = false;
   @Output() closeProfileModal = new EventEmitter<void>()
+
+  ngOnInit(): void {
+    this.connectedUser=this.connectedUserService.getCurrentUser();
+  }
+
   closeModal(){
     this.closeProfileModal.emit();
   }
